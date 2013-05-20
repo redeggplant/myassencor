fc17468cf16c11f0898654221d0401d7ad87c28f
redeggplant <regplant@gmail.com>
2013-05-20 23:31:17 +0400

    Initial commit with base auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assencor::Application.config.secret_token = '0bd3591b29ca06e74440233756cd5d65e72072e069de0fc2ae80ac9eaa923b942f3f54ccecdf0d3cfa0e9b38af7a330f3eb8cd2b7dd1ef71fc98793cba07f076'
